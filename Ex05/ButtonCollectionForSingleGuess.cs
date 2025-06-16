848dc3fc925e62f50100cb8d06297990cb1d4310
EitanVeryKatz <eitanka@mta.ac.il>
2025-06-16 17:25:52 +0300

    finished setup form

--- Ex05/ButtonCollectionForSingleGuess.cs
+++ Ex05/ButtonCollectionForSingleGuess.cs
@@ -34,35 +34,41 @@ namespace Ex05
             Buttons.Add(m_ResaultButton3);
             Buttons.Add(m_ResaultButton4);
 
-            m_ChoiseButton1.Left = i_left + 20;
-            m_ChoiseButton2.Left = m_ChoiseButton1.Right + 16;
-            m_ChoiseButton3.Left = m_ChoiseButton2.Right + 16;
-            m_ChoiseButton4.Left = m_ChoiseButton3.Right + 16;
-            m_SubmitButton.Left = m_ChoiseButton4.Right + 20;
-            m_ResaultButton1.Left = m_SubmitButton.Right + 16;
-            m_ResaultButton3.Left = m_SubmitButton.Right + 16;
-            m_ResaultButton2.Left = m_ResaultButton1.Right + 8;
-            m_ResaultButton4.Left = m_ResaultButton1.Right + 8;
+            m_ChoiseButton1.Left = i_left + 12;
+            m_ChoiseButton2.Left = m_ChoiseButton1.Right + 1;
+            m_ChoiseButton3.Left = m_ChoiseButton2.Right + 1;
+            m_ChoiseButton4.Left = m_ChoiseButton3.Right + 1;
+            m_SubmitButton.Left = m_ChoiseButton4.Right + 1;
+            m_ResaultButton1.Left = m_SubmitButton.Right + 1;
+            m_ResaultButton3.Left = m_ResaultButton1.Right + 1;
+            m_ResaultButton2.Left = m_SubmitButton.Right + 1; 
+            m_ResaultButton4.Left = m_ResaultButton1.Right + 1;
 
-            m_ChoiseButton1.Top = i_Height + 10;
-            m_ChoiseButton2.Top = i_Height + 10;
-            m_ChoiseButton3.Top = i_Height + 10;
-            m_ChoiseButton4.Top = i_Height + 10;
-            m_SubmitButton.Top = i_Height + 20;
-            m_ResaultButton1.Top = i_Height + 10;
-            m_ResaultButton3.Top = i_Height + 10;
-            m_ResaultButton2.Top = m_ResaultButton1.Top + 8;
-            m_ResaultButton4.Top = m_ResaultButton1.Top + 8;
+            m_ChoiseButton1.Top = i_Height + 1;
+            m_ChoiseButton2.Top = i_Height + 1;
+            m_ChoiseButton3.Top = i_Height + 1;
+            m_ChoiseButton4.Top = i_Height + 1;
+            m_SubmitButton.Top = i_Height + 1;
+            m_ResaultButton1.Top = i_Height + 1;
+            m_ResaultButton3.Top = i_Height + 1;
+            m_ResaultButton2.Top = m_ResaultButton1.Bottom + 1;
+            m_ResaultButton4.Top = m_ResaultButton1.Bottom + 1;
 
             m_ChoiseButton1.Width = 20;
             m_ChoiseButton2.Width = 20;
             m_ChoiseButton3.Width = 20;
             m_ChoiseButton4.Width = 20;
-            m_SubmitButton.Width = 20;  
+            m_SubmitButton.Width = 50;  
             m_ResaultButton1.Width = 20;
             m_ResaultButton2.Width = 20;
             m_ResaultButton3.Width = 20;
             m_ResaultButton4.Width = 20;
+
+            m_ResaultButton1.Text = "1";
+            m_ResaultButton2.Text = "2";
+            m_ResaultButton3.Text = "3";
+            m_ResaultButton4.Text = "4";
+            m_SubmitButton.Text = "-->";
         }
 
     }
