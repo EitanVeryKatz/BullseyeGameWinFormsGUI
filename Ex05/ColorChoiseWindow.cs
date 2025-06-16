b23f2ef4aa4529861bad54f604d376c3a52d60c0
EitanVeryKatz <eitanka@mta.ac.il>
2025-06-16 18:19:10 +0300

    updated choise using visual designer

--- Ex05/ColorChoiseWindow.cs
+++ Ex05/ColorChoiseWindow.cs
@@ -11,50 +11,121 @@ namespace Ex05
     internal class ColorChoiseWindow:Form
     {
 
-        private Button OptionBtnRed = new Button();
-        private Button OptionBtnGreen = new Button();
-        private Button OptionBtnBlue = new Button();
-        private Button OptionBtnYellow = new Button();
-        private Button OptionBtnPurple = new Button();
-        private Button OptionBtnGray = new Button();
-        private Button OptionBtnBrown = new Button();
-        private Button OptionBtnOrange = new Button();
-
-        public ColorChoiseWindow()
-        {
-            Controls.Add(OptionBtnRed);
-            Controls.Add(OptionBtnGreen);
-            Controls.Add(OptionBtnBlue);
-            Controls.Add(OptionBtnYellow);
-            Controls.Add(OptionBtnPurple);
-            Controls.Add(OptionBtnGray);
-            Controls.Add(OptionBtnOrange);
-            Controls.Add(OptionBtnBrown);
-
-
-
-            StartPosition = FormStartPosition.CenterParent;
-            OptionBtnBlue.BackColor = Color.Blue;
-            OptionBtnGreen.BackColor = Color.Green;
-            OptionBtnRed.BackColor = Color.Red;
-            OptionBtnYellow.BackColor = Color.Yellow;
-            OptionBtnPurple.BackColor = Color.Purple;
-            OptionBtnGray.BackColor = Color.Gray;
-            OptionBtnBrown.BackColor = Color.Brown;
-            OptionBtnOrange.BackColor = Color.Orange;
-        }
+        private Button OptionBtnRed;
+        private Button OptionBtnGreen;
+        private Button OptionBtnBlue;
+        private Button OptionBtnYellow;
+        private Button OptionBtnPurple;
+        private Button OptionBtnGray;
+        private Button OptionBtnBrown;
+        private Button OptionBtnOrange;
 
         private void InitializeComponent()
         {
+            this.OptionBtnRed = new System.Windows.Forms.Button();
+            this.OptionBtnGreen = new System.Windows.Forms.Button();
+            this.OptionBtnBlue = new System.Windows.Forms.Button();
+            this.OptionBtnYellow = new System.Windows.Forms.Button();
+            this.OptionBtnPurple = new System.Windows.Forms.Button();
+            this.OptionBtnGray = new System.Windows.Forms.Button();
+            this.OptionBtnBrown = new System.Windows.Forms.Button();
+            this.OptionBtnOrange = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // OptionBtnRed
+            // 
+            this.OptionBtnRed.BackColor = System.Drawing.Color.Red;
+            this.OptionBtnRed.Location = new System.Drawing.Point(115, 19);
+            this.OptionBtnRed.Name = "OptionBtnRed";
+            this.OptionBtnRed.Size = new System.Drawing.Size(30, 30);
+            this.OptionBtnRed.TabIndex = 0;
+            this.OptionBtnRed.UseVisualStyleBackColor = false;
+            // 
+            // OptionBtnGreen
+            // 
+            this.OptionBtnGreen.BackColor = System.Drawing.Color.Green;
+            this.OptionBtnGreen.Location = new System.Drawing.Point(62, 19);
+            this.OptionBtnGreen.Name = "OptionBtnGreen";
+            this.OptionBtnGreen.Size = new System.Drawing.Size(30, 30);
+            this.OptionBtnGreen.TabIndex = 1;
+            this.OptionBtnGreen.UseVisualStyleBackColor = false;
+            // 
+            // OptionBtnBlue
+            // 
+            this.OptionBtnBlue.BackColor = System.Drawing.Color.Blue;
+            this.OptionBtnBlue.Location = new System.Drawing.Point(62, 70);
+            this.OptionBtnBlue.Name = "OptionBtnBlue";
+            this.OptionBtnBlue.Size = new System.Drawing.Size(30, 30);
+            this.OptionBtnBlue.TabIndex = 2;
+            this.OptionBtnBlue.UseVisualStyleBackColor = false;
+            // 
+            // OptionBtnYellow
+            // 
+            this.OptionBtnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.OptionBtnYellow.Location = new System.Drawing.Point(12, 70);
+            this.OptionBtnYellow.Name = "OptionBtnYellow";
+            this.OptionBtnYellow.Size = new System.Drawing.Size(30, 30);
+            this.OptionBtnYellow.TabIndex = 3;
+            this.OptionBtnYellow.UseVisualStyleBackColor = false;
+            // 
+            // OptionBtnPurple
+            // 
+            this.OptionBtnPurple.BackColor = System.Drawing.Color.Purple;
+            this.OptionBtnPurple.Location = new System.Drawing.Point(168, 19);
+            this.OptionBtnPurple.Name = "OptionBtnPurple";
+            this.OptionBtnPurple.Size = new System.Drawing.Size(30, 30);
+            this.OptionBtnPurple.TabIndex = 4;
+            this.OptionBtnPurple.UseVisualStyleBackColor = false;
+            // 
+            // OptionBtnGray
+            // 
+            this.OptionBtnGray.BackColor = System.Drawing.Color.Gray;
+            this.OptionBtnGray.Location = new System.Drawing.Point(115, 70);
+            this.OptionBtnGray.Name = "OptionBtnGray";
+            this.OptionBtnGray.Size = new System.Drawing.Size(30, 30);
+            this.OptionBtnGray.TabIndex = 5;
+            this.OptionBtnGray.UseVisualStyleBackColor = false;
+            // 
+            // OptionBtnBrown
+            // 
+            this.OptionBtnBrown.BackColor = System.Drawing.Color.Brown;
+            this.OptionBtnBrown.Location = new System.Drawing.Point(12, 19);
+            this.OptionBtnBrown.Name = "OptionBtnBrown";
+            this.OptionBtnBrown.Size = new System.Drawing.Size(30, 30);
+            this.OptionBtnBrown.TabIndex = 7;
+            this.OptionBtnBrown.UseVisualStyleBackColor = false;
+            // 
+            // OptionBtnOrange
+            // 
+            this.OptionBtnOrange.BackColor = System.Drawing.Color.Orange;
+            this.OptionBtnOrange.Location = new System.Drawing.Point(168, 70);
+            this.OptionBtnOrange.Name = "OptionBtnOrange";
+            this.OptionBtnOrange.Size = new System.Drawing.Size(30, 30);
+            this.OptionBtnOrange.TabIndex = 6;
+            this.OptionBtnOrange.UseVisualStyleBackColor = false;
             // 
             // ColorChoiseWindow
             // 
-            this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(984, 435);
+            this.ClientSize = new System.Drawing.Size(225, 129);
+            this.Controls.Add(this.OptionBtnRed);
+            this.Controls.Add(this.OptionBtnGreen);
+            this.Controls.Add(this.OptionBtnBlue);
+            this.Controls.Add(this.OptionBtnYellow);
+            this.Controls.Add(this.OptionBtnPurple);
+            this.Controls.Add(this.OptionBtnGray);
+            this.Controls.Add(this.OptionBtnOrange);
+            this.Controls.Add(this.OptionBtnBrown);
             this.Name = "ColorChoiseWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.ResumeLayout(false);
 
         }
+    
+        public ColorChoiseWindow()
+        {
+            InitializeComponent();
+        }
+
+       
     }
 }
